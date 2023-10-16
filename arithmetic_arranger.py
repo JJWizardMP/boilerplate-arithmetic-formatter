e17831f4ca8643460b917a7e29f1d16ff0e6aba1
JJWizardMP <76183456+JJWizardMP@users.noreply.github.com>
2023-10-16 18:08:16 +0000

    refactor: adding manage control errors and optimize strings formats

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,65 +1,71 @@
 '''
 @File    :   arithmetic_arranger.py
-@Time    :   2021/11/113 18:38:01
+@Time    :   2023/10/16 13:38:01
 @Author  :   JJwizardMP
-@Version :   1.0.1
-@License :   GLP 2
+@Version :   1.0.2
+@License :   GLP v.2
 @Desc    :   Solution to FCC Python Project: Arithmetic Formatter
 '''
+
+
 def arithmetic_arranger(*problems):
   OF = arithForm(problems[0])
-  flag = True if (len(problems)>1) else False
+  flag = len(problems) > 1
   arranged_problems = OF.build_operation(flag)
   return arranged_problems
 
+
 class operFormat:
-    def __init__ (self, string):
-        self.a, self.s, self.b = string.split()
-        self.lmax = max(len(self.a), len(self.b))
-        self.res = str(int(self.a) + int(self.b)) if self.s == "+" else str(int(self.a) - int(self.b))
+
+  def __init__(self, string):
+    self.a, self.s, self.b = string.split()
+    self.lmax = max(len(self.a), len(self.b))
+    self.res = str(
+        int(self.a) + int(self.b) if self.s == "+" else int(self.a) -
+        int(self.b))
+
 
 class arithForm:
-    def __init__(self, problems):
-        self.dict = {
-            1: "Error: Too many problems.",
-            2: "Error: Operator must be '+' or '-'.",
-            3: "Error: Numbers must only contain digits.",
-            4: "Error: Numbers cannot be more than four digits."
-        }
-        self.problems = problems
-        self.lsformt = []
-    
-    def build_operation(self, flag=False):
-        err = self.test_errors()
-        return self.dict[err] if(err) else self.create_output(flag)
 
-    def test_errors(self):
-        if (len(self.problems) > 5):
-            return 1
-        for problm in self.problems:
-            A, S, B = problm.split()
-            if (not (S == "+" or S == "-")):
-                return 2
-            elif (not (A.isdecimal() and B.isdecimal())):
-                return 3
-            elif (len(A) > 4 or len(B) > 4):
-                return 4
-            else:
-                pass
-        return 0
+  def __init__(self, problems):
+    self.dict_err = {
+        1: "Error: Too many problems.",
+        2: "Error: Operator must be '+' or '-'.",
+        3: "Error: Numbers must only contain digits.",
+        4: "Error: Numbers cannot be more than four digits."
+    }
+    self.problems = problems.copy()
+    self.list_format = []
 
-    def create_output(self, flag=False):
-        m1, m2, rs, dash = ("", "", "", "")
-        self.lsformt = [ operFormat(probl) for probl in self.problems ]
-        for obj in self.lsformt:
-            m1 += " "*(2 + (obj.lmax - len(obj.a))) + obj.a + " "*4
-            m2 += obj.s + " "*(1 + (obj.lmax - len(obj.b))) + obj.b + " "*4
-            dash += "-"*2 + "-"*obj.lmax + " "*4
-            if (flag):
-                rs += " "*(2+(obj.lmax - len(obj.res))) + obj.res + " "*4
-        m1 = m1.rstrip().replace("-", " ")
-        m2 = m2.rstrip()
-        dash = dash.rstrip()
-        if (flag):
-            rs = "\n" + rs.rstrip()
-        return m1 + "\n" + m2 + "\n" + dash + rs
+  def build_operation(self, flag=False):
+    try:
+      self.validate_problems()
+      return self.create_output(flag)
+    except ValueError as e:
+      return str(e)
+
+  def validate_problems(self):
+    if len(self.problems) > 5:
+      raise ValueError(self.dict_err[1])
+    for problem in self.problems:
+      A, S, B = problem.split()
+      if S not in ("+", "-"):
+        raise ValueError(self.dict_err[2])
+      if not (A.isdecimal() and B.isdecimal()):
+        raise ValueError(self.dict_err[3])
+      if len(A) > 4 or len(B) > 4:
+        raise ValueError(self.dict_err[4])
+
+  def create_output(self, flag=False):
+    m1, m2, dash, rs, sep = ("", "", "", "\n", " " * 4)
+    self.list_format = [operFormat(problem) for problem in self.problems]
+    for obj in self.list_format:
+      m1 += f'{obj.a:>{obj.lmax + 2}}{sep}'
+      m2 += f'{obj.s:<2}{obj.b:>{obj.lmax}}{sep}'
+      dash += f"{'-' * (obj.lmax + 2)}{sep}"
+      if (flag):
+        rs += f'{obj.res:>{obj.lmax + 2}}{sep}'
+    # Elimnate white spaces to right side
+    m1, m2, dash, rs = [s.rstrip() for s in (m1, m2, dash, rs)]
+    arith_format = f"{m1}\n{m2}\n{dash}{rs}"
+    return arith_format
